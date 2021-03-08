55de404d83f1ec360fbf332842a1f943479d6750
Juan José Cermeño Portet <jjcerpo@gmail.com>
2021-03-08 22:04:50 +0100

    added test to shopper model

--- sequra_api/spec/models/shopper_spec.rb
+++ sequra_api/spec/models/shopper_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Shopper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:shopper) { create(:shopper) }
+  let(:id) { '1' }
+
+  before do
+    shopper.stub(:id).and_return(id)
+    shopper.stub(:shopper_id).and_return(id)
+  end
+  # Association test
+  # ensure Shopper model has a m:m relationship with the Categories and Search models
+  it { expect have_many(:orders) }
+  # Validation tests
+  it { expect validate_uniqueness_of(:id) }
+  it { expect validate_uniqueness_of(:shopper_id) }
 end
